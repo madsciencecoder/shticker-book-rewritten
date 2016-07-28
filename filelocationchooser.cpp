578217ba98d53ae1651ef2ba875ced18dfdf7b4e
madsciencecoder <madsciencecoder@gmail.com>
2016-07-28 12:47:07 -0400

    Add license comments to files missing it.

--- filelocationchooser.cpp
+++ filelocationchooser.cpp
@@ -1,3 +1,23 @@
+/*
+ * Copyright (c) 2015-2016 Joshua Snyder
+ * Distributed under the GNU GPL v3. For full terms see the file LICENSE
+ *
+ * This file is part of Shticker Book Rewritten.
+ *
+ * Shticker Book Rewritten is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Shticker Book Rewritten is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Shticker Book Rewritten.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "filelocationchooser.h"
 #include "globaldefines.h"
 #include "ui_filelocationchooser.h"
